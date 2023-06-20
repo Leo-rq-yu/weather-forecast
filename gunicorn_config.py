ff1ffa408b8b73f4c8d3dc93be4c389735bc59b0
LEO4388 <ruiqing6.illinois.edu>
2023-06-20 15:25:51 -0500

    Update Gunicorn and Nginx

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -1,3 +1,26 @@
+import multiprocessing
 
+# The address and port to bind to
 bind="0.0.0.0:8080"
-workers = 2
+
+# The number of worker processes for handling requests
+workers = multiprocessing.cpu_count() * 2 + 1
+
+# The type of worker process to use
+worker_class = "gthread"
+
+# The maximum number of requests a worker will process before restarting
+max_requests = 500
+
+# The maximum number of simultaneous clients a worker will accept
+backlog = 2048
+
+# The timeout for graceful worker restarts
+timeout = 30
+
+# The path to your Flask application instance
+app = "app:app"
+
+# Logging configuration
+accesslog = "-"  # Log to stdout
+errorlog = "-"   # Log to stderr
